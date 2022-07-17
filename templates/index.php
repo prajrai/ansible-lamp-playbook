36c361fadb1993de2e0434ee7945f8e6e620fd73
Prajwal <prajraihtc@gmail.com>
2022-07-17 21:31:35 +0530

    first commit

---
+++ templates/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Lamp intro sample</title>
+    </head>
+    <body>
+
+    <?php
+    $color = "red";
+    echo "Hello World!";
+    echo "You have connected to EC2 instance from AWS on IP: 18.117.82.49"
+    ?>
+    </body>
+</html>
